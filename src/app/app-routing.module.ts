4d193944243913a8f07130e290659862c8dae19e
BuithanhtungHUB <buithanhtung100496@gmail.com>
2021-09-02 20:02:39 +0700

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MasterComponent} from "./components/layouts/master/master.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  component: MasterComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
