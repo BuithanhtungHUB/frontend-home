20e62c5657614247c72fd16024a932d240f200ed
BuithanhtungHUB <buithanhtung100496@gmail.com>
2021-09-02 23:24:35 +0700

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {MasterComponent} from "./components/layouts/master/master.component";
 
-const routes: Routes = [{
-  path: 'home',
-  component: MasterComponent
+const routes: Routes = [
+  {
+  path: '',
+  component: MasterComponent,
+  children: [
+    {
+      path: '',
+      loadChildren: () => import('./components/houses/houses.module').then(m => m.HousesModule)
+    }
+  ]
 }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
