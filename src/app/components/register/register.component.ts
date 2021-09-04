ab5a5c296d69fe7478e857c8ae107ec61f67dc67
BuithanhtungHUB <buithanhtung100496@gmail.com>
2021-09-04 15:47:40 +0700

    done register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
 
@@ -26,28 +26,47 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.formRegister = this.fb.group({
-      user_name: [''],
-      phone: [''],
-      role: [''],
-      email: [''],
-      password: [''],
-      password_confirmaiton: [''],
-    })
+      user_name: ['',[Validators.required]],
+      phone: ['',[Validators.required,Validators.pattern(/(0)+[0-9]{9}\b/)]],
+      role: ['', [Validators.required]],
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(6),Validators.maxLength(8)]],
+      password_confirmation: ['',[Validators.required]],
+    });
   }
 
   submit() {
     let data = this.formRegister?.value;
+
     this.authService.register(data).subscribe(respone => {
       this.router.navigate(['/login']).then(respone => {
         this.messageSuccess = 'Đăng ký thành công';
         console.log(this.messageSuccess);
       })
     }, errors => {
-      this.messageError = errors.error;
-      this.messagePasswordConfirm = errors.error.password;
-
-      console.log(errors.error);
+      this.messageError = JSON.stringify(errors.error);
+      this.messagePasswordConfirm = JSON.stringify(errors.error.password) ;
+      this.messageRole = JSON.stringify(errors.error.role);
+      console.log( this.messagePasswordConfirm, this.messageRole, this.messageError);
     } )
   }
 
+  get user_name() {
+    return this.formRegister?.get('user_name');
+  }
+  get phone() {
+    return this.formRegister?.get('phone');
+  }
+  get role() {
+    return this.formRegister?.get('role');
+  }
+  get email() {
+    return this.formRegister?.get('email');
+  }
+  get password() {
+    return this.formRegister?.get('password');
+  }
+  get password_confirmation() {
+    return this.formRegister?.get('password_confirmation');
+  }
 }
